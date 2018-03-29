02330494390937af604e60e292f0513dbda474a3
Girish-Mehta <girish.mehta100@gmail.com>
2018-03-29 09:10:29 +0530

    updaed code

--- config-server/src/main/java/com/kkd/config/configserver/ConfigServerApplication.java
+++ config-server/src/main/java/com/kkd/config/configserver/ConfigServerApplication.java
@@ -1,15 +1,15 @@
-package com.user.authentication.eurekaserver;
+package com.kkd.config.configserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
-@EnableEurekaServer
+@EnableConfigServer
 @SpringBootApplication
-public class EurekaServerApplication {
+public class ConfigServerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(EurekaServerApplication.class, args);
+		SpringApplication.run(ConfigServerApplication.class, args);
 	}
 }
