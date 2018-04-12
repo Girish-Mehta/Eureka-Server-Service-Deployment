7398e15bad6158deb734fe982522824e9f9814bc
Girish-Mehta <girish.mehta100@gmail.com>
2018-04-12 17:04:39 +0530

    Updated zuul security code

--- zuul-gateway/src/main/java/com/user/authentication/zuulgateway/ZuulLoggingFilter.java
+++ zuul-gateway/src/main/java/com/user/authentication/zuulgateway/ZuulLoggingFilter.java
@@ -22,9 +22,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
-import com.netflix.zuul.ZuulFilter;
-import com.netflix.zuul.context.RequestContext;
-import com.netflix.zuul.exception.ZuulException;
 
 @Component
 public class ZuulLoggingFilter extends ZuulFilter {
@@ -32,8 +29,10 @@ public class ZuulLoggingFilter extends ZuulFilter {
 
 	@Value("${Authorities}")
 	String Authorities;
+	
 	@Value("${loginService}")
 	String loginService;
+	
 	@Value("${publicUser}")
 	String publicUser;
 
@@ -41,11 +40,8 @@ public class ZuulLoggingFilter extends ZuulFilter {
 	public Object run() throws ZuulException {
 		RequestContext ctx = RequestContext.getCurrentContext();
 		HttpServletRequest request = ctx.getRequest();
-
 		logger.info(String.format("%s request to %s", request.getMethod(), request.getRequestURL().toString()));
-
-		RequestContext ctx = RequestContext.getCurrentContext();
-		HttpServletRequest request = ctx.getRequest();
+		
 		// extract service request from url
 		String[] urlParts = request.getServletPath().split("/");
 		String serviceUrl = urlParts[1];
